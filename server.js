a2cd81bc3872ae2acfb4ee193678d352fe65c991
Will Griffiths <williamgriffithsireland@hotmail.com>
2025-04-09 20:55:06 +0100

    modify filepath

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url + '.html');
+  let filePath = path.join(__dirname, 'www', req.url === '/' ? 'index.html' : req.url + '.html');
   console.log(`filepath requested -  ${filePath}`);
   fs.readFile(filePath, (err, content) => {
     if (err) {
