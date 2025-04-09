2c2c05dc3a6fbd0b34a7b46ef490f3cb883421a8
Will Griffiths <williamgriffithsireland@hotmail.com>
2025-04-09 22:17:22 +0100

    changes

--- server.js
+++ server.js
@@ -1,8 +1,58 @@
 const uuid = require('uuid');  // Load the UUID library
+const Learnosity = require('../../../index'); // Learnosity SDK constructor
 
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
+const learnositySdk = new Learnosity(); // Instantiate the SDK
+const request = learnositySdk.init(  // Set Learnosity init options
+    'items',                              // Select Items API
+    {
+        consumer_key: process.env.CONSUMER_KEY,
+        domain: 'skillstack-81wm.onrender.com',
+    },
+    process.env.CONSUMER_SECRET,
+    {
+        user_id: generateUUID(),
+        activity_template_id: 'quickstart_examples_activity_template_001',
+        session_id: generateUUID(),
+        activity_id: "quickstart_examples_activity_001",
+        rendering_type: 'assess',
+        type: 'submit_practice',
+        name: "Items API Quickstart",
+        state: 'initial',
+        config: {
+            regions: 'main'
+        }
+    })
+
+
+let renderPage = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>SkillStack - Test Assessment</title>
+  <script src="//items.learnosity.com/?latest-lts"></script>
+</head>
+<body>
+  <h1>Demo test 1</h1>
+  <div id="learnosity_assess"></div>
+</body>
+<script>
+    const request = ${request};
+    const itemsApp = LearnosityItems.init(request, {
+        readyListener() {
+            console.log('ready');
+        },
+        errorListener(err) {
+            console.log('error', err);
+        }
+    })
+</script>
+</html>
+`
 
 const server = http.createServer((req, res) => {
   let filePath = path.join(__dirname, 'www', req.url === '/' ? 'index.html' : req.url);
@@ -18,7 +68,7 @@ const server = http.createServer((req, res) => {
       }
     } else {
       res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content, 'utf-8');
+      res.end(renderPage, 'utf-8');
     }
   });
 });
