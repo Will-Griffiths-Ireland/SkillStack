494ceb3afd277a945a15f1d8d81675f046bb8e9d
Will Griffiths <williamgriffithsireland@hotmail.com>
2025-04-09 20:37:06 +0100

    uuid

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const http = require('http');
-const hostname = '0.0.0.0';
-const port = 3000;
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello, World!</h1>');
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url + '.html');
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found - Wahhhhhhh</h1>', 'utf-8');
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
